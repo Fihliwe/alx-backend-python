d6e411c4248f62dce16d7f63518057b753ceba25
Fihliwe <siyabulelmncube@gmail.com>
2024-05-07 08:46:41 +0000

    Async Generator

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+'''
+Module for async generator
+'''
+
+
+import asyncio
+import random
+
+async def async_generator():
+    '''
+     coroutine will loop 10 times, each time asynchronously wait 1 second
+    '''
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
+        
