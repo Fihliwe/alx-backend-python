310072240846f053b13e872e57631b77dd8bbf99
Fihliwe <siyabulelmncube@gmail.com>
2024-05-08 12:09:59 +0000

    init commit

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,17 +1,14 @@
 #!/usr/bin/env python3
-'''
-Module for async generator
-'''
 
+""" Async Comprehensions """
 
-import asyncio
-import random
+from asyncio import sleep
+from random import uniform
+from typing import Generator
+
 
 async def async_generator():
-    '''
-     coroutine will loop 10 times, each time asynchronously wait 1 second
-    '''
+    """ Async Generator """
     for _ in range(10):
-        await asyncio.sleep(1)
-        yield random.uniform(0, 10)
-        
+        await sleep(1)
+        yield uniform(0, 10)
--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,17 +1,15 @@
 #!/usr/bin/env python3
 
-'''
-Module for Async Comprehensions
-'''
+""" Async Comprehensions """
 
-import asyncio
+from asyncio import sleep
+from random import uniform
+from typing import List
+
 async_generator = __import__('0-async_generator').async_generator
 
-async def async_comprehension():
-    '''
-    collect 10 random numbers using an async comprehensing over async_generator
-    '''
-    number = [i async for i in async_generator()]
-    
-    return number
-    
+
+async def async_comprehension() -> List[float]:
+    """ Async Comprehensions  """
+    a = [i async for i in async_generator()]
+    return a
