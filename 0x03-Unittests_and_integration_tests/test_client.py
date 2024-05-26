ed26d101057b71118333d899b181bc0adae14d98
Fihliwe <siyabulelmncube@gmail.com>
2024-05-26 19:19:18 +0000

    has_license

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -62,7 +62,17 @@ class TestGithubOrgClient(unittest.TestCase):
         self.assertEqual(result, ["repo1", "repo2", "repo3"])
         mock_public_repos_url.assert_called_once()
         mock_get_json.assert_called_once_with("https://api.github.com/orgs/test-org/repos")
-
-
+    
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False)
+    ])   
+    def test_has_license(self, repo, license_key, expected):
+        """
+        method for test_has_license
+        """
+        client = GithubOrgClient("some_org")
+        assert client.has_license(repo, license_key) == expected
+        
 if __name__ == "__main__":
     unittest.main()
